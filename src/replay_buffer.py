a154641802cc62418f064998285d38114bf9c9f2
constantpi <wada.zkai@gmail.com>
2024-02-23 03:10:39 +0900

    優先度付き経験再生

---
+++ src/replay_buffer.py
@@ -0,0 +1,62 @@
+"""
+Prioritized Experience Replayを実現するためのメモリクラス.
+"""
+import numpy as np
+import torch
+
+class PrioritizedReplayBuffer(object):
+    def __init__(self, buffer_size):
+        self.buffer_size = buffer_size
+        self.index = 0
+        self.buffer = []
+        self.priorities = np.zeros(buffer_size, dtype=np.float32)
+        self.priorities[0] = 1.0
+
+    def __len__(self):
+        return len(self.buffer)
+
+    # 経験をリプレイバッファに保存する． 経験は(obs, action, reward, next_obs, done)の5つ組を想定
+    def push(self, experience):
+        if len(self.buffer) < self.buffer_size:
+            self.buffer.append(experience)
+        else:
+            self.buffer[self.index] = experience
+
+        # 優先度は最初は大きな値で初期化しておき, 後でサンプルされた時に更新する
+        self.priorities[self.index] = self.priorities.max()
+        self.index = (self.index + 1) % self.buffer_size
+
+    def sample(self, batch_size, alpha=0.6, beta=0.4):
+        # 現在経験が入っている部分に対応する優先度を取り出し, サンプルする確率を計算
+        priorities = self.priorities[: self.buffer_size if len(self.buffer) == self.buffer_size else self.index]
+        priorities = priorities ** alpha
+        prob = priorities / priorities.sum()
+
+        # >> 演習: 確率probに従ってサンプルする経験のインデックスを用意しましょう
+        # ヒント: np.random.choice などが便利です
+        indices = np.random.choice(a=range(len(self.buffer)),size=batch_size,p=prob)
+        indices = np.random.choice(len(self.buffer), batch_size, p=prob)
+
+        # >> 演習: 上式の通りに重点サンプリングの補正のための重みを計算してみましょう
+        weights = (len(self.buffer)*prob[indices])**(-beta)
+        weights /= weights.max()
+
+        # 上でサンプルしたインデックスに基づいて経験をサンプルし, (obs, action, reward, next_obs, done)に分ける
+        obs, action, reward, next_obs, done = zip(*[self.buffer[i] for i in indices])
+
+        # あとで計算しやすいようにtorch.Tensorに変換して(obs, action, reward, next_obs, done, indices, weights)の7つ組を返す
+        return (torch.stack(obs),
+                torch.as_tensor(action),
+                torch.as_tensor(reward, dtype=torch.float32),
+                torch.stack(next_obs),
+                torch.as_tensor(done, dtype=torch.uint8),
+                indices,
+                torch.as_tensor(weights, dtype=torch.float32))
+
+    # 優先度を更新する. 優先度が極端に小さくなって経験が全く選ばれないということがないように, 微小値を加算しておく.
+    def update_priorities(self, indices, priorities):
+        # print(priorities)
+        # print(indices)
+        # print(self.priorities[indices])
+        # print(priorities)
+        self.priorities[indices] = priorities + 1e-4
