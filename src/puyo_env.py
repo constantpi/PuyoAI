024156c8d2e1ff853a36488d64ed18e6f3e83f46
constantpi <wada.zkai@gmail.com>
2024-02-25 15:49:06 +0900

    board_historyを順に画像にして保存する

--- src/puyo_env.py
+++ src/puyo_env.py
@@ -2,6 +2,7 @@ from puyo_game import PuyoGame
 
 import numpy as np
 import torch
+from PIL import Image, ImageDraw, ImageFont
 
 
 class PuyoEnv:
@@ -35,6 +36,37 @@ class PuyoEnv:
 
     def render(self) -> None:
         print(self.game)
+
+    def save_image(self, num=0) -> int:
+        board_history = self.game.board_history
+        colors = [(255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
+        font = ImageFont.truetype("NotoSansCJK-Regular.ttc", 30)
+        for board, puyo, next_puyo, rensa in board_history:
+            img = Image.new('RGB', (self.width*60+100, (self.height)*60), (255, 255, 255))
+            draw = ImageDraw.Draw(img)
+            print(board, board.shape)
+            for i in range(self.width):
+                for j in range(1, self.height):
+                    color = board[i][j]
+                    draw.rectangle((i*60, (j-1)*60, (i+1)*60, j*60), fill=(255, 255, 255), outline=(0, 0, 0))
+                    if color != 0:
+                        draw.ellipse((i*60+10, (j-1)*60+10, (i+1)*60-10, j*60-10), fill=colors[color], outline=(0, 0, 0))
+            if rensa:
+                draw.text((self.width*60+5, 10), f'連鎖: {rensa}', fill=(0, 0, 0), font=font)
+
+            for i in range(2):
+                for j in range(2):
+                    color = puyo[j]
+                    x = self.width*60 + 30
+                    y = 100 + i*200 + j*60
+                    # 縦に並べる
+                    draw.rectangle((x, y, x+60, y+60), fill=(255, 255, 255), outline=(0, 0, 0))
+                    if color != 0:
+                        draw.ellipse((x+10, y+10, x+50, y+50), fill=colors[color], outline=(0, 0, 0))
+                puyo = next_puyo
+            img.save(f'./images/{num}.png')
+            num += 1
+            print(num)
 
 
 if __name__ == '__main__':
